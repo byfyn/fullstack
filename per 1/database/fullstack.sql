b8f75497c9acf691e373713b59e859e5fc190a38
byfyn <alfinpintar25@gmail.com>
2025-06-24 22:30:20 +0700

    Add fullstack database backup and initial project setup

---
+++ per 1/database/fullstack.sql
@@ -0,0 +1,8 @@
+127.0.0.1/fullstack/		http://localhost/phpmyadmin/index.php?route=/database/sql&db=fullstack
+Your SQL query has been executed successfully.
+
+SHOW CREATE DATABASE fullstack;
+
+
+
+fullstack	CREATE DATABASE `fullstack` /*!40100 DEFAULT CHARA...	
